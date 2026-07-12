aeb3656d029dc4bd888bcd84fd7fd4291056b2ce
Alphaprime-cyber <primealpha818@gmail.com>
2026-07-12 21:37:45 +0100

    Create js/favourites.js

---
+++ js/favourites.js
@@ -0,0 +1,11 @@
+import { auth } from "./firebase.js";
+
+document.getElementById("favouritesList").innerHTML = `
+<div class="card">
+
+<h2>❤️ Favourite Providers</h2>
+
+<p>This feature will display providers you have saved.</p>
+
+</div>
+`;
